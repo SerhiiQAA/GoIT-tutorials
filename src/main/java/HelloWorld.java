8570f50a66bdc7dd549c72162085efb75360db88
SerhiiQA <nabukve@gmail.com>
2025-01-08 19:43:09 +0200

    New

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GoIT world!");
+        System.out.println("I`m Serhi, I`m QA Engineer. And I`m realle love testing field)");
     }
 }
